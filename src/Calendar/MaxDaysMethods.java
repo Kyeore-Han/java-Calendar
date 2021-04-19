f8c6e677dbd4952f285f6d6b051df11f3e39e316
Kyeore_Han <Kyeore_Han@DESKTOP-GOHL1RN>
2021-04-19 16:11:20 +0900

    Using repeat for loop

--- src/Calendar/MaxDaysMethods.java
+++ src/Calendar/MaxDaysMethods.java
@@ -2,15 +2,16 @@ package Calendar;
 
 import java.util.Scanner;
 
-public class MaxDaysMethods {
-	
-	private static final int[] Maxdays= {31,28,31,30,31,30,31,31,30,31,30,31};
-	
-	public int getmaxDaysOfmonth(int month) { //input도 있고 output도 있는 메소드 // 반드시 불러오고 싶으면 parameter를 입력해줘야 하는 형식
-		return Maxdays[month-1];
+public class MaxDaysMethods { // ctrl + shift + f -> 자동 정렬
+
+	private static final int[] Maxdays = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+	public int getmaxDaysOfmonth(int month) { // input도 있고 output도 있는 메소드 // 반드시 불러오고 싶으면 parameter를 입력해줘야 하는 형식
+		return Maxdays[(month - 1)];
 	}
-	public static void printSampleCalender() {  // input도 없고 output도 없는 단순메소드 //불러오면 안에 있는 내용 실행
-		
+
+	public void printSampleCalender() { // input도 없고 output도 없는 단순메소드 //불러오면 안에 있는 내용 실행
+
 		System.out.println(" 일 월 화 수 목 금 토\n");
 		System.out.println("-------------------------");
 		System.out.printf("%2d %2d %2d %2d %2d %2d %2d\n", 1, 2, 3, 4, 5, 6, 7);
@@ -20,19 +21,26 @@ public class MaxDaysMethods {
 		System.out.printf("%2d %2d %2d %2d %2d %2d %2d\n", 29, 30, 31, 1, 2, 3, 4);
 
 	}
+
 	public static void main(String[] args) {
-		System.out.println("최대일수를 출력할 달을 입력하시오.");
 		Scanner scan = new Scanner(System.in);
-		int month = scan.nextInt();
-		
 		MaxDaysMethods cal = new MaxDaysMethods();
-		
-		System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getmaxDaysOfmonth(month)); // 해당 클래스의 메소드를 불러와서 활용(parameter input이 있는 형태)
-		
+
+		System.out.println("입력할 횟수를 입력하시오");
+		int repeat = scan.nextInt();
+
+		for (int i = 0; i < repeat; i++) {
+			System.out.println("최대일수를 출력할 달을 입력하시오.");
+			int month = scan.nextInt();
+			System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getmaxDaysOfmonth(month)); // 해당 클래스의 메소드를 불러와서
+																							// 활용(parameter)
+
+		}
+
 		cal.printSampleCalender();
-		
+
 		scan.close();
-		
+
 	}
 
 }
