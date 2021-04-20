ce421192efa46d0214b2afa314eaaa8d0e853de1
Kyeore_Han <Kyeore_Han@DESKTOP-GOHL1RN>
2021-04-20 23:28:50 +0900

    add apply blank day

--- src/Calendar/Calender.java
+++ src/Calendar/Calender.java
@@ -20,19 +20,39 @@ public class Calender {
 			return Maxdays[(month - 1)];
 		}
 	}
+	public int getblankDaysOfmonth(String weekday) {
+		
+		if (weekday.equals("su")) return 0;
+		else if (weekday.equals("mo")) return 1;
+		else if (weekday.equals("tu")) return 2;
+		else if (weekday.equals("we")) return 3;
+		else if (weekday.equals("th")) return 4;
+		else if (weekday.equals("fr")) return 5;
+		else if (weekday.equals("sa")) return 6;
+		else return 0;
+	}
 
-	public void printCalenderMethod(int year, int month) {
+	public void printCalenderMethod(int year, int month, String weekday) {
 		System.out.printf("   << %4d 년%3d 월 >>\n", year, month);
 		System.out.println(" SU MO TU WE TH FR SA");
 		System.out.println(" --------------------");
 		int maxDay = getmaxDaysOfmonth(year, month);
+		int weekNum = getblankDaysOfmonth(weekday);
+		for (int j = 0; j <= weekNum-1; j++) {
+			if (weekNum == 0) {
+				break;
+			}
+			System.out.print("   ");
+		}
+		
 		for (int i = 1; i <= maxDay; i++) {
 			System.out.printf("%3d", i);
-			if (i % 7 == 0) {
+			if ((i + weekNum) % 7 == 0) {
 				System.out.println();
 			}
 		}
 		System.out.println();
-	}
+		}
+		
 
 }
--- src/Calendar/Prompt.java
+++ src/Calendar/Prompt.java
@@ -11,17 +11,26 @@ public class Prompt {
 		Calender pcal = new Calender();
 		int year = 0;
 		int month = 0;
+		String weekday ="";
+		
 		while (true) {
-			System.out.println("년도를 입력하시오");
+			System.out.println("년도를 입력하시오.");
 			System.out.print(PROMPT);
 			year = scan.nextInt();
-			System.out.println("달을 입력하시오");
-			System.out.print(PROMPT);
-			month = scan.nextInt(); // 다음입력을 기다리기 때문에 print문이 무한히 빠르게 출력되지 않는다.
-			if (year == -1 || month == -1) {
+			if (year == -1) {
 				break;
 			}
-			pcal.printCalenderMethod(year, month);
+			System.out.println("달을 입력하시오.");
+			System.out.print(PROMPT);
+			month = scan.nextInt(); // 다음입력을 기다리기 때문에 print문이 무한히 빠르게 출력되지 않는다.
+			if (month == -1) {
+				break;
+			}
+			System.out.println("첫째날의 요일을 입력하시오.(su, mo, tu, we, th, fr, sa)");
+			System.out.print(PROMPT);
+			weekday = scan.next();
+
+			pcal.printCalenderMethod(year, month, weekday);
 
 		}
 		scan.close();
